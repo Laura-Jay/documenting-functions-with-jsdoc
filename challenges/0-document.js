4e8a5c3f43f84b75737d0a545bbea678df299211
laura J <laura@academy.com>
2022-02-21 12:49:40 +0000

    added JSDoc to asNegative function

--- challenges/0-document.js
+++ challenges/0-document.js
@@ -1,5 +1,11 @@
 // TODO (Ex.0): document and write tests for this function
 
+/**
+ * Given a number, returns the negative version of the number.
+ * @param {number} n - input number
+ * @returns {number} `n` in negative form
+ */
+
 function asNegative(n) {
   // ...
 }
