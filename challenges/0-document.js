73a5bfbbbac61ab000c7840c5e64bb3ab405927d
laura J <laura@academy.com>
2022-02-21 13:18:23 +0000

    added JSDoc to 0-document.js

--- challenges/0-document.js
+++ challenges/0-document.js
@@ -7,7 +7,14 @@
  */
 
 function asNegative(n) {
-  // ...
+  if(n<=0){
+   return n; 
+  } else {
+    return n * -1;
+  }
 }
 
+console.log("asNegative(5):", asNegative(5), "should be -5")
+console.log("asNegative(0):", asNegative(0), "should be 0")
+console.log("asNegative(-7):", asNegative(-7), "should be -7")
 
